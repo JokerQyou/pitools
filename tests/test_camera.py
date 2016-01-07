3b064d6933ef7e910fab5634420358562866f1bc
Joker Qyou <Joker.Qyou@gmail.com>
2016-01-07 21:11:56 +0800

    Add test

---
+++ tests/test_camera.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+from __future__ import unicode_literals
+import unittest
+import tempfile
+import shutil
+
+from flask import Flask
+from pitools import camera
+
+app = Flask(__name__)
+app.register_blueprint(camera.blueprint)
+
+
+class CameraTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.workspace = tempfile.mkdtemp()
+        self.app = app.test_client()
+
+    def tearDown(self):
+        shutil.rmtree(self.workspace)
+
+    def test_post_shot_api(self):
+        '''
+        Should fail with 405 method not allowed
+        '''
+        rv = self.app.post('/camera/shot')
+        assert 405 == rv.status_code
+
+    def test_get_shot_api(self):
+        '''
+        Should return a image with image/* MIME
+        '''
+        rv = self.app.get('/camera/shot')
+        assert rv.content_type.startswith('image/')
+        print dir(rv)
+
+if __name__ == '__main__':
+    unittest.main()
