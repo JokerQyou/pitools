72d460b2b4eadfd5e1e2ece35d77da153e1c7822
Joker Qyou <Joker.Qyou@gmail.com>
2016-01-08 20:51:51 +0800

    Update server script

--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 
 from flask import Flask
 from pitools import camera
+from pitools.sensors import bmp085
 
 app = Flask(__name__)
 app.register_blueprint(camera.blueprint)
+app.register_blueprint(bmp085.blueprint, url_prefix='/sensors')
 
 
 def serve():
